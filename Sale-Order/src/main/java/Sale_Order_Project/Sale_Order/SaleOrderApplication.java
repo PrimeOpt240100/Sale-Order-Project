7d8b9b77f641a253e9d00edec7b0fbbdb13853ab
Manish Sharma <msgurugram240100@gmail.com>
2024-08-09 22:56:12 +0530

    first commit

---
+++ Sale-Order/src/main/java/Sale_Order_Project/Sale_Order/SaleOrderApplication.java
@@ -0,0 +1,13 @@
+package Sale_Order_Project.Sale_Order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleOrderApplication.class, args);
+	}
+
+}
---
+++ Sale-Order/src/test/java/Sale_Order_Project/Sale_Order/SaleOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package Sale_Order_Project.Sale_Order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
